9a2e58920b7f06e7d24e41b4825c6d4bfe61cdb7
deepmogal18 <hackerdeepm@gmail.com>
2025-01-21 18:23:30 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 18092005
+let accountEmail = "deep@google.com"
+var accountPassword = "12345"
+accountCity = "Nashik"
+
+accountEmail = "deepmogal@google.com"
+accountPassword = "09090"
+accountCity ="ozar"
+
+
+// accountId = 2
+console.log(accountId)
+console.table([ accountId , accountEmail ,accountPassword, accountCity,]);
